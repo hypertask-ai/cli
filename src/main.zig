a7a463a4886cdf2f1e8fafbb5d4fff5ebe674daf
Valentin Yeo <valentin@hypertask.ai>
2026-09-17 20:45:48 +0200

    HTPR-6500 [INFRA] Bump CLI release version to 0.2.3

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,7 @@ const output = @import("output.zig");
 const router = @import("router.zig");
 const token_refresh = @import("token_refresh.zig");
 
-const version = "0.2.2 (zig)";
+const version = "0.2.3 (zig)";
 
 pub fn main() void {
     run() catch |err| {
